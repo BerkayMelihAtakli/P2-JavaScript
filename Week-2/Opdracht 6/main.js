56362ad013515aceb1c5db6329ba0ae48c3992f2
BerkayAtakli <9023046@student.zadkine.nl>
2025-11-28 11:32:34 +0100

    `.

---
+++ Week-2/Opdracht 6/main.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen() {
+    const titel = document.getElementById("h1");
+    titel.style.color = 'red';
+    titel.textContent = "Inhoud aangepast!";
+    console.log(titel)
+}
+
+kleur_aanpassen();
