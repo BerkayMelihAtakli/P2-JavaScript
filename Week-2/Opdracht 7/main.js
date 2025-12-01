03af01c9e413826a3570adb28554c67b2048b5bd
BerkayMelihAtakli <9023046@student.zadkine.nl>
2025-12-01 22:17:30 +0100

    blabla

---
+++ Week-2/Opdracht 7/main.js
@@ -0,0 +1,9 @@
+function kleur_aanpassen() {
+    const titel = document.getElementById("h1");
+    titel.style.color = 'red';
+    titel.textContent = "Inhoud aangepast!";
+    console.log(titel)
+}
+
+
+document.getElementById("h1").onclick = function() {kleur_aanpassen()};
