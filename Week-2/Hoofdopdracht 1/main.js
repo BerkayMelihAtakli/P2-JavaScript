fcc323b8da4383ef743b1a196b026765307f6d9a
BerkayMelihAtakli <9023046@student.zadkine.nl>
2025-12-01 23:52:06 +0100

    11

--- Week-2/Hoofdopdracht 1/main.js
+++ Week-2/Hoofdopdracht 1/main.js
@@ -1,16 +1,29 @@
 function LightMode() {
 const titel = document.getElementById("h1");
+const LightMode = document.getElementById("LightMode");
+const DarkMode = document.getElementById("DarkMode");
 document.body.style.backgroundColor = 'orange';
 titel.textContent = "Good Morningg ☀️!";
 titel.style.color = 'black';
+LightMode.style.backgroundColor = "orange";
+LightMode.style.color = "black";
+DarkMode.style.backgroundColor = "";
+DarkMode.style.color = "";
+
 }
 
 
 function DarkMode() {
 const titel = document.getElementById("h1");
+const LightMode = document.getElementById("LightMode");
+const DarkMode = document.getElementById("DarkMode");
 document.body.style.backgroundColor = 'black';
 titel.textContent = "Good Night 🌙!";
 titel.style.color = 'white';
+DarkMode.style.backgroundColor = "red";
+DarkMode.style.color = "white";
+LightMode.style.backgroundColor = "";
+LightMode.style.color = "";
 }
 
 
