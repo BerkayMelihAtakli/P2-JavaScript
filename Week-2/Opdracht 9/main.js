50da8e9ee1d5bce336f33358d1dbe16354668b64
BerkayMelihAtakli <9023046@student.zadkine.nl>
2025-12-01 23:35:59 +0100

    qwe

--- Week-2/Opdracht 9/main.js
+++ Week-2/Opdracht 9/main.js
@@ -3,12 +3,10 @@ const titel = document.getElementById("h1");
 titel.style.display = 'none';
 }
 
-document.getElementById("verberg_titel").onclick = function() {verberg_titel()};
+
 
 function toon_tekst() {
 const tekst = document.getElementById("h1");
 tekst.style.display = 'block';
 tekst.style.backgroundColor = 'yellow';
 }
-
-document.getElementById("toon_tekst").onclick = function() {toon_tekst()};
